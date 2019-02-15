2a02d31cf73641fe44aaee9dd37ce4f6a52f018e
maximpristupa <g.maxim.pristupa@gmail.com>
2019-02-15 13:12:01 +0300

    app created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,29 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(function() {
+	var modal = document.getElementById('myModal');
+	var btn = document.getElementsByClassName("myBtn");
+	var info = document.getElementById("info");
+	var upd = document.getElementById("update");
+	var span = document.getElementsByClassName("close")[0];
+	var modalContent = document.getElementsByClassName("modal-content")[0];
+	[...document.querySelectorAll(".myBtn")].map(
+		function(element, index, array) {
+			element.onclick = function() {
+			  modal.style.display = "block";
+			  info.style.display = "block";
+			  upd.style.display = "none";
+			  var oImg = document.createElement("img");
+			  oImg.setAttribute('src', 'assets' + element.id );
+		  	  var item = document.getElementById("user_throw");
+		  	  item.replaceChild(oImg, item.childNodes[0]);
+			}
+		}
+	);
+	span.onclick = function() {
+	  modal.style.display = "none";
+	 	modalContent.style.height = "470px";
+		modalContent.style.width = "880px";  
+	};
+});
